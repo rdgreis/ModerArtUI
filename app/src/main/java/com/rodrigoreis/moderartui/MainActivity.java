be4c5d1d39ed790a408b2ceb3b5955608a147516
Rodrigo Reis <rodrigo@rodrigoreis.com>
2015-03-23 01:11:23 -0300

    Working with the SeekBar widget.

--- app/src/main/java/com/rodrigoreis/moderartui/MainActivity.java
+++ app/src/main/java/com/rodrigoreis/moderartui/MainActivity.java
@@ -4,25 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Space;
+import android.view.SurfaceView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends ActionBarActivity {
+
+    private SeekBar mSeekBar;
+    public SurfaceView box1;
+    private SurfaceView box2;
+    private SurfaceView box3;
+    private SurfaceView box4;
+    private SurfaceView box5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Space box1 = (Space) findViewById(R.id.box1);
-        Space box2 = (Space) findViewById(R.id.box2);
-        Space box3 = (Space) findViewById(R.id.box3);
-        Space box4 = (Space) findViewById(R.id.box4);
-        Space box5 = (Space) findViewById(R.id.box5);
+        final SurfaceView box1 = (SurfaceView) findViewById(R.id.box1);
+        final SurfaceView box2 = (SurfaceView) findViewById(R.id.box2);
+        final SurfaceView box3 = (SurfaceView) findViewById(R.id.box3);
+        final SurfaceView box4 = (SurfaceView) findViewById(R.id.box4);
+        final SurfaceView box5 = (SurfaceView) findViewById(R.id.box5);
 
-        box1.bringToFront();
+        mSeekBar = (SeekBar)findViewById(R.id.seekbar);
+        //mSeekBar.setOnSeekBarChangeListener(this);
+
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,6 +40,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
